0acfb7aa733d9f0b45e0b81da8432973eca3d079
Nurdaulet Satbergen <nurik93sat@gmail.com>
2025-05-02 12:54:50 +0500

    refactored service

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
 import { CreatePostCredentials, Post } from './post.model';
 import { Subject } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
@@ -7,29 +7,30 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
   providedIn: 'root'
 })
 export class PostsService {
-  posts = signal<Post[]>([]);
+  private readonly _posts = signal<Post[]>([]);
+  posts = computed(() => this._posts());
 
-  add$ = new Subject<CreatePostCredentials>();
-  remove$ = new Subject<string>();
-  like$ = new Subject<string>();
+  readonly add$ = new Subject<CreatePostCredentials>();
+  readonly remove$ = new Subject<string>();
+  readonly like$ = new Subject<string>();
 
   constructor() {
     this.add$
       .pipe(takeUntilDestroyed())
       .subscribe(value => {
-        this.posts.update(posts => [...posts, this.creatPost(value)]);
+        this._posts.update(posts => [...posts, this.creatPost(value)]);
       });
 
     this.remove$
       .pipe(takeUntilDestroyed())
       .subscribe(id => {
-        this.posts.update(posts => posts.filter(post => post.id!==id));
+        this._posts.update(posts => posts.filter(post => post.id!==id));
       })
 
     this.like$
       .pipe(takeUntilDestroyed())
       .subscribe(id => {
-        this.posts.update(posts =>
+        this._posts.update(posts =>
           posts.map(post =>
               post.id === id
                 ? { ...post, likes_count: post.likes_count + 1 }
