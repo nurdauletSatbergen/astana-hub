171b1de3ac6eab9f005b53989aee7eb1888b6da3
Nurdaulet Satbergen <nurik93sat@gmail.com>
2025-05-02 12:50:33 +0500

    renamed pipe

--- src/app/shared/pipes/filter-by-title.pipe.ts
+++ src/app/shared/pipes/filter-by-title.pipe.ts
@@ -1,9 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filterByName'
+  name: 'filterByTitle'
 })
-export class FilterByNamePipe implements PipeTransform {
+export class FilterByTitlePipe implements PipeTransform {
   transform<T extends { title: string }>(value: T[] = [], searchName: string): T[] {
     if (!searchName) return value;
     return value.filter(item => item.title.toLowerCase().includes(searchName.toLowerCase()));
